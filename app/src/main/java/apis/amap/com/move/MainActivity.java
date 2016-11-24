b4cd6178cdafe07c902a0e46f85029f51920f783
guibao.gao <guibao.ggb@alibaba-inc.com>
2016-11-24 17:59:45 +0800

    平滑移动更新

--- app/src/main/java/apis/amap/com/move/MainActivity.java
+++ app/src/main/java/apis/amap/com/move/MainActivity.java
@@ -2,136 +2,49 @@ package apis.amap.com.move;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptor;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.LatLngBounds;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
-import com.amap.api.maps.model.Polyline;
 import com.amap.api.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    private MapView mMapView;
-    private AMap mAmap;
-    private Polyline mVirtureRoad;
-    private Marker mMoveMarker;
-
-    private boolean mIsRunning=true;
-
-    // 通过设置间隔时间和距离可以控制速度和图标移动的距离
-    private static final int TIME_INTERVAL = 80;
-    private static final double DISTANCE = 0.0001;
+    private MapView mapView;
+    private AMap aMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMapView = (MapView) findViewById(R.id.map);
 
-        mMapView.onCreate(savedInstanceState);
-        mAmap = mMapView.getMap();
-        initRoadData();
-        moveLooper();
-    }
-
-    private void initRoadData() {
-        double centerLatitude = 39.916049;
-        double centerLontitude = 116.399792;
-        double deltaAngle = Math.PI / 180 * 5;
-        double radius = 0.02;
-        PolylineOptions polylineOptions = new PolylineOptions();
-        for (double i = 0; i < Math.PI * 2; i = i + deltaAngle) {
-            float latitude = (float) (-Math.cos(i) * radius + centerLatitude);
-            float longtitude = (float) (Math.sin(i) * radius + centerLontitude);
-            polylineOptions.add(new LatLng(latitude, longtitude));
-            if (i > Math.PI) {
-                deltaAngle = Math.PI / 180 * 30;
-            }
-        }
-        float latitude = (float) (-Math.cos(0) * radius + centerLatitude);
-        float longtitude = (float) (Math.sin(0) * radius + centerLontitude);
-        polylineOptions.add(new LatLng(latitude, longtitude));
-
-        polylineOptions.width(10);
-        polylineOptions.color(Color.RED);
-        mVirtureRoad = mAmap.addPolyline(polylineOptions);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.setFlat(true);
-        markerOptions.anchor(0.5f, 0.5f);
-        markerOptions.icon(BitmapDescriptorFactory
-                .fromResource(R.drawable.marker));
-        markerOptions.position(new LatLng(latitude,longtitude));
-        mMoveMarker = mAmap.addMarker(markerOptions);
-        mMoveMarker.setRotateAngle((float) getAngle(0));
-
-        LatLngBounds.Builder builder=new LatLngBounds.Builder();
-        builder.include(new LatLng(39.896049, 116.379792));
-        builder.include(new LatLng(39.936049, 116.419792));
-        mAmap.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(),10));
-
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        init();
     }
 
     /**
-     * 根据点获取图标转的角度
+     * 初始化AMap对象
      */
-    private double getAngle(int startIndex) {
-        if ((startIndex + 1) >= mVirtureRoad.getPoints().size()) {
-            throw new RuntimeException("index out of bonds");
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
         }
-        LatLng startPoint = mVirtureRoad.getPoints().get(startIndex);
-        LatLng endPoint = mVirtureRoad.getPoints().get(startIndex + 1);
-        return getAngle(startPoint, endPoint);
-    }
-
-    /**
-     * 根据两点算取图标转的角度
-     */
-    private double getAngle(LatLng fromPoint, LatLng toPoint) {
-        double slope = getSlope(fromPoint, toPoint);
-        if (slope == Double.MAX_VALUE) {
-            if (toPoint.latitude > fromPoint.latitude) {
-                return 0;
-            } else {
-                return 180;
-            }
-        }
-        float deltAngle = 0;
-        if ((toPoint.latitude - fromPoint.latitude) * slope < 0) {
-            deltAngle = 180;
-        }
-        double radio = Math.atan(slope);
-        double angle = 180 * (radio / Math.PI) + deltAngle - 90;
-        return angle;
-    }
-
-    /**
-     * 根据点和斜率算取截距
-     */
-    private double getInterception(double slope, LatLng point) {
-
-        double interception = point.latitude - slope * point.longitude;
-        return interception;
-    }
-
-
-
-    /**
-     * 算斜率
-     */
-    private double getSlope(LatLng fromPoint, LatLng toPoint) {
-        if (toPoint.longitude == fromPoint.longitude) {
-            return Double.MAX_VALUE;
-        }
-        double slope = ((toPoint.latitude - fromPoint.latitude) / (toPoint.longitude - fromPoint.longitude));
-        return slope;
 
     }
 
@@ -141,7 +54,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        mMapView.onResume();
+        mapView.onResume();
+
     }
 
     /**
@@ -150,7 +64,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
-        mMapView.onPause();
+        mapView.onPause();
+
     }
 
     /**
@@ -159,7 +74,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mMapView.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 
     /**
@@ -168,101 +83,199 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mIsRunning=false;
-        mMapView.onDestroy();
-    }
-
-    /**
-     * 计算每次移动的距离
-     */
-    private double getMoveDistance(double slope) {
-        if (slope == Double.MAX_VALUE||slope==0) {
-            return DISTANCE;
-        }
-        return Math.abs((DISTANCE * slope) / Math.sqrt(1 + slope * slope));
-    }
-
-    /**
-     * 判断是否为反序
-     * */
-    private boolean isReverse(LatLng startPoint, LatLng endPoint, double slope){
-        if(slope==0){
-            return	startPoint.longitude>endPoint.longitude;
-        }
-        return (startPoint.latitude > endPoint.latitude);
+        mapView.onDestroy();
 
     }
 
-    /**
-     * 获取循环初始值大小
-     * */
-    private double getStart(LatLng startPoint,double slope){
-        if(slope==0){
-            return	startPoint.longitude;
-        }
-        return  startPoint.latitude;
-    }
+    public void move(View view) {
 
-    /**
-     * 获取循环结束大小
-     * */
-    private double getEnd(LatLng endPoint,double slope){
-        if(slope==0){
-            return	endPoint.longitude;
-        }
-        return  endPoint.latitude;
-    }
+        addPolylineInPlayGround();
 
+        List<LatLng> points = readLatLngs();
+        LatLngBounds bounds = new LatLngBounds(points.get(0), points.get(points.size() - 2));
+        aMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 10));
 
+        SmoothMarker smoothMarker = new SmoothMarker(aMap);
+        smoothMarker.setDescriptor(BitmapDescriptorFactory.fromResource(R.drawable.car));
 
-    /**
-     * 循环进行移动逻辑
-     */
-    public void moveLooper() {
-        new Thread() {
+        LatLng drivePoint = points.get(0);
+        Pair<Integer, LatLng> pair = PointsUtil.calShortestDistancePoint(points, drivePoint);
+        points.set(pair.first, drivePoint);
+        List<LatLng> subList = points.subList(pair.first, points.size());
 
-            public void run() {
-                while (mIsRunning) {
-                    for (int i = 0; i < mVirtureRoad.getPoints().size() - 1; i++) {
+        smoothMarker.setPoints(subList);
+        smoothMarker.setTotalDuration(150000);
 
+        aMap.setInfoWindowAdapter(infoWindowAdapter);
+        smoothMarker.getMarker().showInfoWindow();
 
-                        LatLng startPoint = mVirtureRoad.getPoints().get(i);
-                        LatLng endPoint = mVirtureRoad.getPoints().get(i + 1);
-                        mMoveMarker
-                                .setPosition(startPoint);
-                        mMoveMarker.setRotateAngle((float) getAngle(startPoint,
-                                endPoint));
+        smoothMarker.setMoveListener(new SmoothMarker.SmoothMarkerMoveListener() {
+            @Override
+            public void move(final double distance) {
 
-                        double slope = getSlope(startPoint, endPoint);
-                        boolean isReverse =isReverse(startPoint, endPoint, slope);
-                        double moveDistance = isReverse ? getMoveDistance(slope) : -1 * getMoveDistance(slope);
-                        double intercept = getInterception(slope, startPoint);
-                        for(double j=getStart(startPoint, slope); (j > getEnd(endPoint, slope))==isReverse;j = j
-                                - moveDistance){
-                            LatLng latLng = null;
-                            if(slope==0){
-                                latLng = new LatLng(startPoint.latitude, j);
-                            }
-                            else if (slope == Double.MAX_VALUE) {
-                                latLng = new LatLng(j, startPoint.longitude);
-                            }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (infoWindowLayout != null && title != null) {
 
-                            else {
-
-                                latLng = new LatLng(j, (j - intercept) / slope);
-                            }
-                            mMoveMarker.setPosition(latLng);
-                            try {
-                                Thread.sleep(TIME_INTERVAL);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
+                            title.setText("距离终点还有： " + (int) distance + "米");
                         }
-
                     }
-                }
-            }
+                });
 
-        }.start();
+            }
+        });
+
+        smoothMarker.startSmoothMove();
+
     }
+
+    AMap.InfoWindowAdapter infoWindowAdapter = new AMap.InfoWindowAdapter() {
+        @Override
+        public View getInfoWindow(Marker marker) {
+
+            return getInfoWindowView(marker);
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+
+
+            return getInfoWindowView(marker);
+        }
+    };
+
+    LinearLayout infoWindowLayout;
+    TextView title;
+    TextView snippet;
+
+    private View getInfoWindowView(Marker marker) {
+        if (infoWindowLayout == null) {
+            infoWindowLayout = new LinearLayout(MainActivity.this);
+            infoWindowLayout.setOrientation(LinearLayout.VERTICAL);
+            title = new TextView(MainActivity.this);
+            snippet = new TextView(MainActivity.this);
+            title.setTextColor(Color.BLACK);
+            snippet.setTextColor(Color.BLACK);
+            infoWindowLayout.setBackgroundResource(R.drawable.infowindow_bg);
+
+            infoWindowLayout.addView(title);
+            infoWindowLayout.addView(snippet);
+        }
+
+        return infoWindowLayout;
+    }
+
+    private void addPolylineInPlayGround() {
+        List<LatLng> list = readLatLngs();
+        List<Integer> colorList = new ArrayList<Integer>();
+        List<BitmapDescriptor> bitmapDescriptors = new ArrayList<BitmapDescriptor>();
+
+        int[] colors = new int[]{Color.argb(255, 0, 255, 0), Color.argb(255, 255, 255, 0), Color.argb(255, 255, 0, 0)};
+
+        //用一个数组来存放纹理
+        List<BitmapDescriptor> textureList = new ArrayList<BitmapDescriptor>();
+        textureList.add(BitmapDescriptorFactory.fromResource(R.drawable.custtexture));
+
+        List<Integer> texIndexList = new ArrayList<Integer>();
+        texIndexList.add(0);//对应上面的第0个纹理
+        texIndexList.add(1);
+        texIndexList.add(2);
+
+        Random random = new Random();
+        for (int i = 0; i < list.size(); i++) {
+            colorList.add(colors[random.nextInt(3)]);
+            bitmapDescriptors.add(textureList.get(0));
+
+        }
+
+        aMap.addPolyline(new PolylineOptions().setCustomTexture(BitmapDescriptorFactory.fromResource(R.drawable.custtexture)) //setCustomTextureList(bitmapDescriptors)
+//				.setCustomTextureIndex(texIndexList)
+                .addAll(list)
+                .useGradient(true)
+                .width(18));
+    }
+
+
+    private void addMarkerInBeijing(int number) {
+        Random random = new Random();
+
+        //new LatLng(39.90403, 116.407525)
+        double tem_lat = 39.9;
+        double tem_lon = 116.4;
+        BitmapDescriptor descriptor = BitmapDescriptorFactory.defaultMarker();
+        if (number == 1) {
+            aMap.addMarker(new MarkerOptions().position(new LatLng(tem_lat, tem_lon)).icon(descriptor));
+            return;
+        }
+
+        for (int i = 0; i < number; i++) {
+            LatLng latLng = new LatLng(tem_lat + random.nextDouble() * (random.nextBoolean() ? 1 : -1), tem_lon + random.nextDouble() * (random.nextBoolean() ? 1 : -1));
+            aMap.addMarker(new MarkerOptions().position(latLng).icon(descriptor));
+        }
+
+    }
+
+    private List<LatLng> readLatLngs() {
+        List<LatLng> points = new ArrayList<LatLng>();
+        for (int i = 0; i < coords.length; i += 2) {
+            points.add(new LatLng(coords[i + 1], coords[i]));
+        }
+        return points;
+    }
+
+    private double[] coords = {116.3499049793749, 39.97617053371078,
+            116.34978804908442, 39.97619854213431, 116.349674596623,
+            39.97623045687959, 116.34955525200917, 39.97626931100656,
+            116.34943728748914, 39.976285626595036, 116.34930864705592,
+            39.97628129172198, 116.34918981582413, 39.976260803938594,
+            116.34906721558868, 39.97623535890678, 116.34895185151584,
+            39.976214717128855, 116.34886935936889, 39.976280148755315,
+            116.34873954611332, 39.97628182112874, 116.34860763527448,
+            39.97626038855863, 116.3484658907622, 39.976306080391836,
+            116.34834585430347, 39.976358252119745, 116.34831166130878,
+            39.97645709321835, 116.34827643560175, 39.97655231226543,
+            116.34824186261169, 39.976658372925556, 116.34825080406188,
+            39.9767570732376, 116.34825631960626, 39.976869087779995,
+            116.34822111635201, 39.97698451764595, 116.34822901510276,
+            39.977079745909876, 116.34822234337618, 39.97718701787645,
+            116.34821627457707, 39.97730766147824, 116.34820593515043,
+            39.977417746816776, 116.34821013897107, 39.97753930933358
+            , 116.34821304891533, 39.977652209132174, 116.34820923399242,
+            39.977764016531076, 116.3482045955917, 39.97786190186833,
+            116.34822159449203, 39.977958856930286, 116.3482256370537,
+            39.97807288885813, 116.3482098441266, 39.978170063673524,
+            116.34819564465377, 39.978266951404066, 116.34820541974412,
+            39.978380693859116, 116.34819672351216, 39.97848741209275,
+            116.34816588867105, 39.978593409607825, 116.34818489339459,
+            39.97870216883567, 116.34818473446943, 39.978797222300166,
+            116.34817728972234, 39.978893492422685, 116.34816491505472,
+            39.978997133775266, 116.34815408537773, 39.97911413849568,
+            116.34812908154862, 39.97920553614499, 116.34809495907906,
+            39.979308267469264, 116.34805113358091, 39.97939658036473,
+            116.3480310509613, 39.979491697188685, 116.3480082124968,
+            39.979588529006875, 116.34799530586834, 39.979685789111635,
+            116.34798818413954, 39.979801430587926, 116.3479996420353,
+            39.97990758587515, 116.34798697544538, 39.980000796262615,
+            116.3479912988137, 39.980116318796085, 116.34799204219203,
+            39.98021407403913, 116.34798535084123, 39.980325006125696,
+            116.34797702460183, 39.98042511477518, 116.34796288754136,
+            39.98054129336908, 116.34797509821901, 39.980656820423505,
+            116.34793922017285, 39.98074576792626, 116.34792586413015,
+            39.98085620772756, 116.3478962642899, 39.98098214824056,
+            116.34782449883967, 39.98108306010269, 116.34774758827285,
+            39.98115277119176, 116.34761476652932, 39.98115430642997,
+            116.34749135408349, 39.98114590845294, 116.34734772765582,
+            39.98114337322547, 116.34722082902628, 39.98115066909245,
+            116.34708205250223, 39.98114532232906, 116.346963237696,
+            39.98112245161927, 116.34681500222743, 39.981136637759604,
+            116.34669622104072, 39.981146248090866, 116.34658043260109,
+            39.98112495260716, 116.34643721418927, 39.9811107163792,
+            116.34631638374302, 39.981085081075676, 116.34614782996252,
+            39.98108046779486, 116.3460256053666, 39.981049089345206,
+            116.34588814050122, 39.98104839362087, 116.34575119741586,
+            39.9810544889668, 116.34562885420186, 39.981040940565734,
+            116.34549232235582, 39.98105271658809, 116.34537348820508,
+            39.981052294975264, 116.3453513775533, 39.980956549928244
+    };
 }
---
+++ app/src/main/java/apis/amap/com/move/PointsUtil.java
@@ -0,0 +1,125 @@
+package apis.amap.com.move;
+
+import android.util.Pair;
+
+import com.amap.api.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PointsUtil {
+
+
+    /**
+     * 如果在垂足在端点，直接返回端点的下标
+     *
+     * @param points
+     * @param point
+     * @return
+     */
+    public static Pair<Integer, LatLng> calShortestDistancePoint(List<LatLng> points, LatLng point) {
+        List<DPoint> dPoints = new ArrayList<DPoint>();
+        int index = 0;
+        for (LatLng latLng : points) {
+            dPoints.add(new DPoint(latLng.latitude, latLng.longitude));
+            if (latLng.equals(point)) {
+                return new Pair(index, point);
+            }
+            index++;
+        }
+        DPoint dPoint = new DPoint(point.latitude, point.longitude);
+        Pair<Integer, DPoint> pair = calShortestDistancePoint(dPoints, dPoint);
+        if (pair != null) {
+
+            return new Pair(pair.first, new LatLng(pair.second.x, pair.second.y));
+        }
+        return null;
+    }
+
+    public static Pair<Integer, DPoint> calShortestDistancePoint(List<DPoint> points, DPoint point) {
+        int size = points.size();
+        if (size < 2) {
+            return null;
+        }
+        Pair<Integer, DPoint> result = null;
+        DPoint FP = point;
+
+        DPoint FA = null;
+        DPoint FB = null;
+        double temp = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            if (i == 0) {
+                FA = points.get(i);
+                if (FA.equals(point)) {
+                    return new Pair<Integer, DPoint>(i, point);
+                }
+            } else {
+                FA = FB;
+            }
+            FB = points.get(i + 1);
+            if (FB.equals(point)) {
+                return new Pair<Integer, DPoint>(i + 1, point);
+            }
+            // 使用FPoint
+            Pair<Double, DPoint> pair = pointToSegDist(FP.x, FP.y, FA.x, FA.y, FB.x, FB.y);
+            if (result == null) {
+                temp = pair.first;
+                result = new Pair<Integer, DPoint>(i, pair.second);
+            } else if (temp > pair.first) {
+                temp = pair.first;
+                result = new Pair<Integer, DPoint>(i, pair.second);
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 点P到线段AB的最短距离，不是点到直线的距离<br>
+     * 点P到AB上的垂足为C<br>
+     * 如果C在AB的两侧则，最短距离为PA或PB，否则为PC<br>
+     *
+     * @param x  点P的坐标
+     * @param y
+     * @param x1 点A的坐标
+     * @param y1
+     * @param x2 点B的坐标
+     * @param y2
+     * @return
+     */
+    private static Pair<Double, DPoint> pointToSegDist(double x, double y, double x1, double y1,
+                                                       double x2, double y2) {
+        double cross = (x2 - x1) * (x - x1) + (y2 - y1) * (y - y1);
+        if (cross <= 0)
+            return new Pair<Double, DPoint>(Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1)), new DPoint(x1, y1));
+
+        double d2 = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+        if (cross >= d2)
+            return new Pair<Double, DPoint>(Math.sqrt((x - x2) * (x - x2) + (y - y2) * (y - y2)), new DPoint(x2, y2));
+        ;
+
+        double r = cross / d2;
+        double px = x1 + (x2 - x1) * r;
+        double py = y1 + (y2 - y1) * r;
+        double dis = Math.sqrt((x - px) * (x - px) + (py - y) * (py - y));
+        Pair<Double, DPoint> pair = new Pair<Double, DPoint>(dis, new DPoint(px, py));
+        return pair;
+    }
+
+
+    public static class DPoint {
+        public double x, y;
+
+        public DPoint() {
+
+        }
+
+        public DPoint(double ax, double ay) {
+            x = ax;
+            y = ay;
+        }
+    }
+
+
+}
